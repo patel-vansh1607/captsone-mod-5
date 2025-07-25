94dd8a2a870c7658f0b39837c57e866b2ba4a24c
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-25 11:09:58 +0300

    creating routes for dashboard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
