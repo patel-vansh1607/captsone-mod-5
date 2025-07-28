af6324be71be6f933913a22f8075f1185d1aa1a8
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-28 11:05:06 +0300

    adding the auth

--- login/server/controllers/authControllers.js
+++ login/server/controllers/authControllers.js
@@ -1,13 +1,24 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/userModel')
+const createError = require('../utils/appError');
 //register user
 exports.signup = async (req, res, next) => {
     try {
         const user = await User.findOne({email: req.body.email})
 
         if(user) {
-            return next(new createError);
+            return next(new createError('User already exists', 400));
         }
-    }catch(error)
+        const hashedPassword = await bcrypt.has(req.body.password, 12);
+        const newUser = await User.create({
+            ...req.body,
+            password: hashedPassword
+        })
+
+        //jwt
+    }catch(error){
+        next(error)
+    }
  }
 
 
