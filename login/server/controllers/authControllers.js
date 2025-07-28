5a4f1ac49d5809c73434b92dabde0458d125240b
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-28 10:53:22 +0300

    adding signup auth

--- login/server/controllers/authControllers.js
+++ login/server/controllers/authControllers.js
@@ -1,4 +1,17 @@
 const User = require('../models/userModel')
- exports.signup = async (req, res, next) => { }
+//register user
+exports.signup = async (req, res, next) => {
+    try {
+        const user = await User.findOne({email: req.body.email})
 
+        if(user) {
+            return next(new createError);
+        }
+    }catch(error)
+ }
+
+
+
+
+//login user
   exports.login = async (req, res, next) => { }
