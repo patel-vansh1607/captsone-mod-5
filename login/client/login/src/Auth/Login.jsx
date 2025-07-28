0c5b31d916765ffa488ad02ef0c65012f0e74ca9
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-28 17:56:55 +0300

    login page complete

--- login/client/login/src/Auth/Login.jsx
+++ login/client/login/src/Auth/Login.jsx
@@ -1,34 +1,28 @@
 import React from 'react'
 import { Card, Typography, Form, Input, Button, Flex, Alert, Spin} from 'antd';
 import { Link } from 'react-router-dom';
-import registerimage from "../assets/images.jpeg"
+import loginimage from "../assets/images.jpeg"
 
 const Login = () => {
+
+  const handleLogin = async(values) =>{
+    console.log(values)
+  }
   return (
     <Card className="form-container" >   
       <Flex gap="large" align='center'>
+         <Flex flex={1}>
+          <img src={loginimage} className='auth-image' />
+        </Flex>
         <Flex vertical flex={1}>
           <Typography.Title level={3} className="title">
-          <strong>Create an account</strong>
+          <strong>Sign In</strong>
         </Typography.Title>
         <Typography.Text type="secondary" className="slogan">
-          Join for exclusive access!
+          Unlock your world!
         </Typography.Text>
 
-        <Form layout="vertical" onFinish={handleRegister} autoComplete="off" style={{ marginTop: 20 }}>
-          <Form.Item
-            label="Full Name"
-            name="name"
-            rules={[
-              {
-                required: true,
-                message: 'Please enter your full name',
-              },
-            ]}
-          >
-            <Input size="large" placeholder="Enter your full name" />
-          </Form.Item>
-
+        <Form layout="vertical" onFinish={handleLogin} autoComplete="off">
           <Form.Item
             label="Email"
             name="email"
@@ -58,19 +52,6 @@ const Login = () => {
           >
             <Input.Password size="large" placeholder="Enter your password" />
           </Form.Item>
-
-          <Form.Item
-            label="Confirm Password"
-            name="passwordConfirm"
-            rules={[
-              {
-                required: true,
-                message: 'Please confirm your password',
-              },
-            ]}
-          >
-            <Input.Password size="large" placeholder="Re-enter your password" />
-          </Form.Item>
             {/* {
               error && <Alert description={error} type="error" showIcon closable className='alert' />
             } */}
@@ -82,24 +63,23 @@ const Login = () => {
             block
             className='btn'>
 {/*               {loading ? <Spin /> : 'Create Account'}
- */}            </Button>
+ */}           Sign In
+  </Button>
           </Form.Item>
 
           <Form.Item>
-            <Link to= "/login">
+            <Link to= "/">
             <Button 
             size="large"
             block
             className='btn'>
-              Sign In
+              Create an Account
             </Button>
             </Link>
           </Form.Item>
         </Form>
         </Flex>
-        <Flex flex={1}>
-          <img src={registerimage} className='auth-image' />
-        </Flex>
+       
       </Flex>
       </Card>
   )
