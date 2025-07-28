87473fccf46665897d599fe4f829df00d405e5a3
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-28 11:12:08 +0300

    adding route

--- login/server/index.js
+++ login/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-
+const authRouter = require('./routes/authRoutes');
 // Middleware
 app.use(cors());
 app.use(express.json());
