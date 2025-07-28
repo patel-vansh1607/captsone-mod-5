8c461d4094288b72af76a07e377994cf039edd33
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-29 00:35:08 +0300

    fixing err

---
+++ login/client/login/src/hooks/useLogin.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { useAuth } from '../contexts/AuthContext'
+import { message } from 'antd';
+
+const useLogin = () => {
+  const { login } = useAuth()
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const loginUser = async (values) => {
+    const { email, password } = values; // ✅ FIX HERE
+
+    try {
+      setError(null)
+      setLoading(true)
+
+      const res = await fetch('http://localhost:3000/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+        });
+
+
+      const data = await res.json()
+
+      if (res.status === 201) {
+        message.success(data.message)
+        login(data.token, data.user)
+      } else if (res.status === 400) {
+        setError(data.message)
+      } else {
+        message.error("Login failed")
+      }
+
+    } catch (error) {
+      message.error(error.message || "Something went wrong")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { loading, error, loginUser }
+}
+
+export default useLogin
