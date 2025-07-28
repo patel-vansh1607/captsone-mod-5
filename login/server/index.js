c3139f3796ed374d7eec7b35989990f2fe2a4fb3
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-28 10:37:25 +0300

    initiating error

--- login/server/index.js
+++ login/server/index.js
@@ -7,14 +7,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
+
 //route
+
+
+
+
+
+
 
 //mogoose connection
 mongoose
 .connect('mongodb://127.0.0.1:27017/authentication')
 .then(() => console.log('MongoDB connected'))
 .catch((error)=> console.error('MongoDB connection error:', error));
+
+
+
+
 //global error handler
+app.use((err, res, req, next) => {
+    err.statuCode = err.statuCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statuCode).json{
+        status: err.status,
+        message: err.message,
+    }
+})
+
+
+
 
 //server
 const PORT = 3000;
