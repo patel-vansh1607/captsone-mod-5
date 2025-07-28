9524bf65ba24b64985e4784401c1e1761acc7ea8
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-28 10:24:10 +0300

    adding mongoose connection

---
+++ login/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//route
+
+//mogoose connection
+mongoose
+.connect('mongodb://127.0.0.1:27017/authentication')
+.then(() => console.log('MongoDB connected'))
+.catch((error)=> console.error('MongoDB connection error:', error));
+//global error handler
+
+//server
