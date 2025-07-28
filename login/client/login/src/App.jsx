2a1c390bee543d6e112be20aebfaa0f5e6991426
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-29 00:35:01 +0300

    fixing err

--- login/client/login/src/App.jsx
+++ login/client/login/src/App.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import Register from './Auth/Register'
 import Login from './Auth/Login'
 import Dashboard from './pages/Dashboard'
-
+import { useAuth } from './contexts/AuthContext'
 
 const App = () => {
+  const { isAuthenticated } = useAuth()
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={!isAuthenticated ? <Register /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   )
