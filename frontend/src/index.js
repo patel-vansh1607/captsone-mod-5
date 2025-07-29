daac2a5a0d0510091419b8dceb3b1e6120e67dde
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-29 14:26:52 +0300

    del

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { TaskProvider } from "./context/TaskContent"; // ✅ IMPORT CONTEXT
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  
+]);
+
 root.render(
-  <BrowserRouter>
-    <TaskProvider>
-      <App />
-    </TaskProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
