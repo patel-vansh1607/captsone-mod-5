fb250416d8295ca43f15d9b4bf4b38f15e201958
Vansh Patel <patel.vansh1607@gmail.com>
2025-07-29 11:15:58 +0300

    handling erros

--- login/client/login/src/hooks/useSignUp.js
+++ login/client/login/src/hooks/useSignUp.js
@@ -16,7 +16,7 @@ const useSignUp = () => {
       setError(null)
       setLoading(true)
 
-      const res = await fetch('http://localhost:3000/api/auth/signup', {
+      const res = await fetch('http://localhost:5000/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
