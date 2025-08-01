c993ca0c24709b76f8d6c53de4e9bfa7916436cf
Vansh Patel <patel.vansh1607@gmail.com>
2025-08-01 12:40:51 +0300

    adding links

--- dashboard/src/hooks/useSignUp.js
+++ dashboard/src/hooks/useSignUp.js
@@ -16,7 +16,7 @@ const useSignUp = () => {
       setError(null)
       setLoading(true)
 
-      const res = await fetch('http://localhost:5000/api/auth/signup', {
+      const res = await fetch('https://login-backend-svdi.onrender.com/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
